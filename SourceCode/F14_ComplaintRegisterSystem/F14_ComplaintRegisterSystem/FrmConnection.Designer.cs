82efea65b23187b3b4220d509c90568c6108240b
sazzad <sazzad@SAZZAD_LAPTOP>
2026-04-17 02:50:36 +0700

    Added connection form with database status test and controlled access

--- SourceCode/F14_ComplaintRegisterSystem/F14_ComplaintRegisterSystem/FrmConnection.Designer.cs
+++ SourceCode/F14_ComplaintRegisterSystem/F14_ComplaintRegisterSystem/FrmConnection.Designer.cs
@@ -28,13 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(253, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(272, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Complaint Register System";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(27, 16);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(139, 16);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "Status: Not Connected";
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(30, 45);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(136, 23);
+            this.btnTest.TabIndex = 2;
+            this.btnTest.Text = "Test Connection";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Enabled = false;
+            this.btnEnter.Location = new System.Drawing.Point(173, 45);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(113, 23);
+            this.btnEnter.TabIndex = 3;
+            this.btnEnter.Text = "Enter System";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // FrmConnection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmConnection";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Button btnEnter;
     }
 }
 
--- SourceCode/F14_ComplaintRegisterSystem/F14_ComplaintRegisterSystem/FrmConnection.cs
+++ SourceCode/F14_ComplaintRegisterSystem/F14_ComplaintRegisterSystem/FrmConnection.cs
@@ -7,14 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace F14_ComplaintRegisterSystem
 {
     public partial class FrmConnection : Form
     {
+        string connStr = @"Data Source=SAZZAD_LAPTOP\SQLSERVERDEV;Initial Catalog=ComplaintDB;Integrated Security=True";
+
         public FrmConnection()
         {
             InitializeComponent();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+                    conn.Open();
+
+                    lblStatus.Text = "Status: Connected";
+                    btnEnter.Enabled = true;
+
+                    MessageBox.Show("Database connected successfully.");
+                }
+            }
+            catch (Exception ex)
+            {
+                lblStatus.Text = "Status: Failed";
+                btnEnter.Enabled = false;
+
+                MessageBox.Show("Connection failed: " + ex.Message);
+            }
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Main system form coming next.");
         }
     }
 }
