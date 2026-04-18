7a635ab3d2b79f9956df85ce81fc11873c08a1dc
sazzad <sazzad@SAZZAD_LAPTOP>
2026-04-19 02:44:29 +0700

    Designed user complaint panel interface

--- SourceCode/F14_ComplaintRegisterSystem/F14_ComplaintRegisterSystem/FrmUserPanel.Designer.cs
+++ SourceCode/F14_ComplaintRegisterSystem/F14_ComplaintRegisterSystem/FrmUserPanel.Designer.cs
@@ -28,20 +28,200 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbUser = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbCategory = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnView = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.dgvMyComplaints = new System.Windows.Forms.DataGridView();
+            this.lblInfo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMyComplaints)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Citizen Name:";
+            // 
+            // cmbUser
+            // 
+            this.cmbUser.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbUser.FormattingEnabled = true;
+            this.cmbUser.Location = new System.Drawing.Point(173, 49);
+            this.cmbUser.Name = "cmbUser";
+            this.cmbUser.Size = new System.Drawing.Size(333, 24);
+            this.cmbUser.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Complaint Title: ";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(173, 89);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(330, 22);
+            this.txtTitle.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Description:";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(173, 138);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(330, 106);
+            this.txtDescription.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(531, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Catagory:";
+            // 
+            // cmbCategory
+            // 
+            this.cmbCategory.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCategory.FormattingEnabled = true;
+            this.cmbCategory.Location = new System.Drawing.Point(654, 49);
+            this.cmbCategory.Name = "cmbCategory";
+            this.cmbCategory.Size = new System.Drawing.Size(330, 24);
+            this.cmbCategory.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(534, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Report Date:";
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Location = new System.Drawing.Point(654, 91);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(330, 22);
+            this.dtpDate.TabIndex = 9;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(534, 167);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(127, 23);
+            this.btnSubmit.TabIndex = 10;
+            this.btnSubmit.Text = "Submit Complaint";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // btnView
+            // 
+            this.btnView.Location = new System.Drawing.Point(681, 167);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(138, 23);
+            this.btnView.TabIndex = 11;
+            this.btnView.Text = "View My Complaints";
+            this.btnView.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(840, 166);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // dgvMyComplaints
+            // 
+            this.dgvMyComplaints.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMyComplaints.Location = new System.Drawing.Point(50, 307);
+            this.dgvMyComplaints.Name = "dgvMyComplaints";
+            this.dgvMyComplaints.RowHeadersWidth = 51;
+            this.dgvMyComplaints.RowTemplate.Height = 24;
+            this.dgvMyComplaints.Size = new System.Drawing.Size(934, 277);
+            this.dgvMyComplaints.TabIndex = 13;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(50, 609);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(94, 16);
+            this.lblInfo.TabIndex = 14;
+            this.lblInfo.Text = "Status : Ready";
             // 
             // FrmUserPanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1082, 653);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.dgvMyComplaints);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnView);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.dtpDate);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cmbCategory);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbUser);
+            this.Controls.Add(this.label1);
             this.Name = "FrmUserPanel";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "User Complaint Panel";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMyComplaints)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbUser;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbCategory;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.DataGridView dgvMyComplaints;
+        private System.Windows.Forms.Label lblInfo;
     }
 }
